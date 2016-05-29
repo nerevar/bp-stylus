ad490b71dbc4f7db7d97babb6ea24bbdbf50f841
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2016-05-29 18:56:26 +0300

    refactoring: add server, h5bp, remove jade, axis, rapture, jeet

--- gulpfile.js
+++ gulpfile.js
@@ -4,136 +4,118 @@
 //
 // Plugins
 //
-var
-  gulp       = require('gulp'),
-  plumber    = require('gulp-plumber'),
-  gutil      = require('gulp-util'),
-  notify     = require('gulp-notify'),
-  watch      = require('gulp-watch'),
-  stylus     = require('gulp-stylus'),
-  sourcemaps = require('gulp-sourcemaps'),
-  jade       = require('gulp-jade'),
-  uglify     = require('gulp-uglify'),
-  concat     = require('gulp-concat'),
-  jshint     = require('gulp-jshint'),
-  stylish    = require('jshint-stylish'),
-  refresh    = require('gulp-livereload'),
-  axis       = require('axis'),
-  jeet       = require('jeet'),
-  rupture    = require('rupture');
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    plumber = require('gulp-plumber'),
+    gutil = require('gulp-util'),
+    notify = require('gulp-notify'),
+    watch = require('gulp-watch'),
+    stylus = require('gulp-stylus'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer-stylus'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    axis = require('axis'),
+    jeet = require('jeet'),
+    rupture = require('rupture');
 
 //
 // Sources
 //
 var sources = {
-  images      : 'img/**/*',
-  favicon     : 'favicon.ico',
-  videos      : 'video/**/*',
-  stylus      : 'stylus/**/*',
-  fonts       : 'fonts/**/*',
-  javascripts : 'js/*.js',
-  vendorjs    : [
-    'js/vendor/jquery.waypoints.js',
-    'js/vendor/jquery.ba-hashchange.js',
-    'js/vendor/headroom.js',
-    'js/vendor/jQuery.headroom.js'
-  ],
-  jade        : 'views/**/*'
+    html: 'public/**/*.html',
+    images: 'img/**/*',
+    favicon: 'favicon.ico',
+    stylus: 'stylus/**/*',
+    fonts: 'fonts/**/*',
+    js: 'js/*.js',
+    vendorjs: [
+        'js/vendor/*.js'
+    ]
 };
 
 //
 // Destinations
 //
 var destinations = {
-  root        : '',
-  images      : 'public/img/',
-  videos      : 'public/video',
-  styles      : 'public/css/',
-  fonts       : 'public/fonts/',
-  javascripts : 'public/js/'
+    root: '',
+    images: 'public/img/',
+    styles: 'public/css/',
+    fonts: 'public/fonts/',
+    js: 'public/js/'
 };
 
-//
-// Stylus task
-//
 gulp.task('stylus', function () {
-  gulp.src('./stylus/site.styl')
-  .pipe(plumber({
-    errorHandler: notify.onError({
-        sound: 'Purr',
-        title: "Stylus Error:",
-        message:  "<%= error.message %>"})
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(stylus({
-      use: [axis(), jeet(), rupture()],
-      compress: true,
-      linenos: false
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(destinations.styles))
-    .pipe(refresh());
+    gulp.src('./stylus/site.styl')
+        .pipe(plumber({
+            errorHandler: notify.onError({
+                sound: 'Purr',
+                title: "Stylus Error:",
+                message: "<%= error.message %>"
+            })
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(stylus({
+            use: [
+                // axis(), jeet(), rupture(),
+                autoprefixer('> 1%', 'last 2 versions', 'Android >= 4')
+            ],
+            compress: true,
+            linenos: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(destinations.styles))
+        .pipe(connect.reload());
 });
 
 //
 // Javascript tasks
 //
-gulp.task('scripts', function() {
-  return gulp.src(sources.javascripts)
-    .pipe(jshint()
-      .on('error', gutil.beep))
-    .pipe(jshint.reporter(stylish))
-    .pipe(plumber())
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(destinations.javascripts))
-    .pipe(uglify())
-    .pipe(gulp.dest(destinations.javascripts))
-    .pipe(refresh());
+gulp.task('scripts', function () {
+    return gulp.src(sources.js)
+        .pipe(jshint()
+            .on('error', gutil.beep))
+        .pipe(jshint.reporter(stylish))
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(destinations.js))
+        .pipe(uglify())
+        .pipe(gulp.dest(destinations.js))
+        .pipe(connect.reload());
 });
 
-gulp.task('vendorjs', function() {
-  return gulp.src(sources.vendorjs)
-    .pipe(jshint()
-      .on('error', gutil.beep))
-    .pipe(jshint.reporter(stylish))
-    .pipe(plumber())
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(destinations.javascripts))
-    .pipe(uglify())
-    .pipe(gulp.dest(destinations.javascripts));
+gulp.task('vendorjs', function () {
+    return gulp.src(sources.vendorjs)
+        .pipe(plumber())
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(destinations.js))
+        .pipe(uglify())
+        .pipe(gulp.dest(destinations.js));
 });
 
-//
-// Jade task
-//
-
-gulp.task('jade', function() {
-  gulp.src('views/pages/*')
-    .pipe(jade({}))
-    .pipe(gulp.dest('public/'))
-    .pipe(refresh());
+gulp.task('html', function () {
+    gulp.src(sources.html)
+        .pipe(connect.reload());
 });
 
-//
-// Watch tasks
-//
-gulp.task('watch', ['build'], function() {
-  refresh.listen();
-  gulp.watch(sources.stylus, ['stylus']);
-  gulp.watch(sources.styluschild, ['stylus']);
-  gulp.watch(sources.jade, ['jade']);
-  gulp.watch(sources.javascripts, ['scripts']);
-  gulp.watch(sources.vendorjs, ['vendorjs']);
+gulp.task('watch', ['build'], function () {
+    gulp.watch(sources.stylus, ['stylus']);
+    gulp.watch(sources.html, ['html']);
+    gulp.watch(sources.js, ['scripts']);
+    gulp.watch(sources.vendorjs, ['vendorjs']);
 });
 
-//
-// Task runners
-//
-gulp.task('default', ['build'], function () {
-  process.exit(0);
+gulp.task('server', ['watch'], function () {
+    connect.server({
+        port: 8888,
+        root: 'public',
+        livereload: true
+    });
 });
 
-//
-// Assets Only
-//
 gulp.task('build', ['stylus', 'scripts', 'vendorjs']);
+
+gulp.task('default', ['build']);
+
